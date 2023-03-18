1b96d16dd5200bffd18ce20aa3361cc8ae321665
siqiuchen <Siqiu.Chen@dentsplysirona.com>
2023-03-18 08:20:33 +0800

    update start up script

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,20 +1,16 @@
 import { Probot, createNodeMiddleware, createProbot } from "probot";
 
-import {app} from "../../../src/index";
+import app from "../../../src/index";
 
-// const probot = createProbot(
-//     {
-//         APP_ID: process.env.APP_ID,
-//         PRIVATE_KEY: process.env.PRIVATE_KEY,
-//         WEBHOOK_PROXY_URL: process.env.WEBHOOK_PROXY_URL,
-//         GITHUB_TOKEN: process.env.GITHUB_TOKEN,
-//     },
-// );
 
 const probot = new Probot({
     appId: process.env.APP_ID,
     privateKey: process.env.PRIVATE_KEY,
-    githubToken: process.env.GITHUB_TOKEN,
-});
+    secret: process.env.WEBHOOK_SECRET,
+    webhookPath: "/api/github/webhooks",
+    webhookProxy: process.env.WEBHOOK_PROXY_URL,
+});// Set the appId option for probot
+probot.load(app, { appId: process.env.APP_ID });
 
-export default createNodeMiddleware(app, { probot, webhooksPath: '/api/github/webhooks' });
+export default createNodeMiddleware(app, 
+    { probot, webhooksPath: '/api/github/webhooks' });
