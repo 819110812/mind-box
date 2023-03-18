02a5143abe5e26e3e89f7e05800304181a5e8e05
siqiuchen <Siqiu.Chen@dentsplysirona.com>
2023-03-18 08:43:09 +0800

    delete middleware

--- api/github/webhooks/index.ts
+++ api/github/webhooks/index.ts
@@ -6,14 +6,18 @@ import app from "../../../src/index";
 const APP_ID=305149
 
 
-const probot = new Probot({
+const appIdOptions = {
     appId: APP_ID,
     privateKey: process.env.PRIVATE_KEY,
     secret: process.env.WEBHOOK_SECRET,
-    webhookPath: "/api/github/webhooks",
-    webhookProxy: process.env.WEBHOOK_PROXY_URL,
-});// Set the appId option for probot
-probot.load(app, { appId: process.env.APP_ID });
+}
+
+
+const probot = new Probot({
+    ...appIdOptions,
+});
+
+
 
 export default createNodeMiddleware(app, 
     { probot, webhooksPath: '/api/github/webhooks' });
